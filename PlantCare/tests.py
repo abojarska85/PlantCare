5eb786b24987d0f3729ab73d93b7c323f25ef8b3
Agnieszka Bojarska <ag.bojarska@gmail.com>
2024-06-16 12:08:08 +0200

    testy dla accounts

--- PlantCare/tests.py
+++ PlantCare/tests.py
@@ -1,4 +1,5 @@
 import pytest
+from django.contrib.auth.models import User
 from django.test import TestCase, Client
 from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
@@ -6,6 +7,88 @@ from pytest_django.asserts import assertTemplateUsed
 from PlantCare.forms import AddSoilForm, AddMyPlantForm, AddFertilizationForm, AddWateringForm, AddReplantingForm, \
     SearchForm
 from PlantCare.models import PlantType, Soil, MyPlant, Fertilization, Watering, Replanting
+
+#test dla widoku CreateUser
+def test_create_user_get():
+    url = reverse('login')
+    client = Client()
+    response = client.get(url)
+    assert response.status_code == 200
+@pytest.mark.django_db
+def test_create_user_post_correct_data(): #przy danych poprawnych
+    url = reverse('register')
+    client = Client()
+    data = {
+        'username': 'test',
+        'password': 'password',
+        'password2': 'password'
+    }
+    response = client.post(url, data)
+    assert response.status_code == 302
+    assert User.objects.filter(username='test').exists()
+@pytest.mark.django_db
+def test_create_user_post_wrong_data(): #przy różnych hasłach
+    url = reverse('register')
+    client = Client()
+    data = {
+        'username': 'test',
+        'password': 'password',
+        'password2': 'password2'
+    }
+    response = client.post(url, data)
+    assert response.status_code == 200
+    assert 'Passwords do not match' in response.context['error']
+
+
+# testy dla widoku LoginView
+def test_login_view_get():
+    url = reverse('login')
+    client = Client()
+    response = client.get(url)
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_login_view_post(): #sprawdza status odpowiedzi po próbie zalogowania
+    username = 'test'
+    password = 'password'
+    User.objects.create(username=username, password=password)
+    url = reverse('login')
+    client = Client()
+    data = {
+        'username': username,
+        'password': password
+    }
+    response = client.post(url, data)
+    assert response.status_code == 302 #sprawdza czy jest przekierowanie po udanym logowaniu
+
+@pytest.mark.django_db
+def test_login_view_post_session(): #sprawdza czy użytkownik jest faktycznie zalogowany
+    username = 'test'
+    password = 'password'
+    User.objects.create_user(username=username, password=password)
+    url = reverse('login')
+    client = Client()
+    data = {
+        'username': username,
+        'password': password
+    }
+    response = client.post(url, data, follow=True)
+    print("Status code: ", response.status_code)
+    assert response.status_code == 200
+    assert 'sessionid' in response.client.cookies #sprawdza ciasteczka klienta
+
+
+@pytest.mark.django_db
+def test_logout(): #sprawdza czy sessionid jest puste, po wylogowaniu powinno być puste
+    username = 'test'
+    password = 'password'
+    User.objects.create_user(username=username, password=password)
+    client = Client()
+    client.login(username=username, password=password)
+    assert 'sessionid' in client.cookies and client.cookies['sessionid'].value
+    url = reverse('logout')
+    client.get(url)
+    assert 'sessionid' in client.cookies and not client.cookies['sessionid'].value
 
 
 #model PlantType
@@ -111,6 +194,20 @@ def test_search_plants_view(): #sprawdzenie czy widok jest dostępny i czy w kon
     assert response.status_code == 200
     assert 'form' in response.context
     assert isinstance(response.context['form'], SearchForm)
+    assertTemplateUsed(response, 'plantCare/search_result.html')
+
+
+@pytest.mark.django_db
+def test_search_plants_search_query(my_plant, plant_type):
+    plant_1 = MyPlant.objects.create(name='plant_1', species=plant_type, light_requirements=1, soil_humidity=1, description='test description')
+    plant_2 = MyPlant.objects.create(name='plant_2', species=plant_type, light_requirements=1, soil_humidity=1, description='test description')
+    plant_3 = MyPlant.objects.create(name='plant3', species=plant_type, light_requirements=1, soil_humidity=1, description='test description')
+    url = reverse('search_plant')
+    client = Client()
+    response = client.get(reverse('search_plant'), {'search_query': 'plant_'})
+    assert plant_1 in response.context['object_list']
+    assert plant_2 in response.context['object_list']
+    assert plant_3 not in response.context['object_list']
 
 #test dla widoku UpdateMyPlant:
 @pytest.mark.django_db
@@ -121,6 +218,8 @@ def test_update_my_plant_get(my_plant): #dla niezalogowanego użytkownika
     login_url = reverse('login')
     assert response.status_code == 302
     assert response.url == f"{login_url}?next={url}"
+
+
 @pytest.mark.django_db
 def test_update_my_plant_get_login(user, my_plant): #dla zalogowanego uzytkownika, formularz w kontekście
     url = reverse('update_my_plant', args=[my_plant.pk])
@@ -210,6 +309,8 @@ def test_delete_soil_get(soil): #dla użytkownika niezalogowanego
     assert response.status_code == 302
     login_url = reverse('login')
     assert response.url == f"{login_url}?next={url}"
+
+
 @pytest.mark.django_db
 def test_delete_soil_get_login(user, soil): #dla użytkownika zalogowanego, kontekst, template
     url = reverse('delete_soil', args=[soil.id])
@@ -219,6 +320,7 @@ def test_delete_soil_get_login(user, soil): #dla użytkownika zalogowanego, kont
     assert response.status_code == 200
     assert isinstance(response.context['soil'], Soil)
     assertTemplateUsed(response, "plantCare/soil_delete.html")
+
 
 @pytest.mark.django_db
 def test_delete_soil_post_action(user, soil): #test dla usuwania gdy jest zgoda
@@ -231,6 +333,8 @@ def test_delete_soil_post_action(user, soil): #test dla usuwania gdy jest zgoda
     assert response.url == reverse('show_soil') #sprawdzam przekierowanie
     with pytest.raises(Soil.DoesNotExist): #sprawdzam czy pobranie obiektu spowoduje wyjątek
         Soil.objects.get(pk=soil.pk)
+
+
 @pytest.mark.django_db
 def test_delete_soil_post_no_action(user, soil):
     url = reverse('delete_soil', args=[soil.id])
@@ -242,13 +346,10 @@ def test_delete_soil_post_no_action(user, soil):
     assert Soil.objects.filter(pk=soil.pk).exists() #sprawdzam że obiekt nadal istnieje w bazie
 
 
-
-
-
 #model Replanting
 #test dodawanie przesadzania do widoku AddReplantingView
 @pytest.mark.django_db
-def test_show_replanting(my_plants, soils, replanting): #test chodzi, ale w mojej ocenie jest niepełny
+def test_show_replanting(my_plants, soils, replanting):
     url = reverse('show_replanting')
     client = Client()
     response = client.get(url)
@@ -264,6 +365,8 @@ def test_add_replanting_get(): #dla użytkownika niezalogowanego
     assert response.status_code == 302
     login_url = reverse('login')
     assert response.url == f"{login_url}?next={url}"
+
+
 @pytest.mark.django_db
 def test_add_replanting_get_login(user): #dla użytkownika zalogowanego
     url = reverse('add_replanting')
@@ -293,12 +396,12 @@ def test_add_replanting_post(my_plant, soil, user):
 
 #model Watering
 @pytest.mark.django_db
-def test_show_watering(my_plants, watering): #test chodzi, ale można by było jeszcze coś dodatkowo przetestowć
+def test_show_watering(watering): #test chodzi, ale można by było jeszcze coś dodatkowo przetestowć
     url = reverse('show_watering')
     client = Client()
     response = client.get(url)
     assert response.status_code == 200
-    assert response.context['watering'].count() == len(my_plants)
+    assert response.context['watering'].count() == len(watering)
     for w in watering:
         assert w in response.context['watering']
 
